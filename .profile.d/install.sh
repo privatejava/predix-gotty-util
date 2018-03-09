9d5c21fee07e007c4d80b19720495ea39715e96b
Narayan G. Maharjan <privatejava@yahoo.com>
2018-03-09 16:16:09 +0545

    gotty extract issue
    Extract Directory was not set so there was an issue.

--- .profile.d/install.sh
+++ .profile.d/install.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+echo 'Downloading..'
 curl -s -L -o "$HOME/linux_amd64.tar.gz" "https://github.com/yudai/gotty/releases/download/v0.0.11/linux_amd64.tar.gz"
-tar zxf "$HOME/linux_amd64.tar.gz"
+echo 'Extracting..'
+tar zvxf "$HOME/linux_amd64.tar.gz" -C $HOME
+echo 'Extracting complete..'
 chmod 755 "$HOME/gotty"
